bea8c9a445eee76ae9ae21a6618e80c527ae9920
rabidgremlin <jack@rabidgremlin.com>
2014-11-14 16:28:03 +1300

    Complete Vagrant config and packtag path issue

--- README.md
+++ README.md
@@ -34,6 +34,18 @@ App should work with any modern servlet engine. Steps to deploy are:
 4. Copy the opwas.war file into your container's web apps folder
 5. Configure your container with a system property named _opwas.config.file_ and set it to the location of the opwas.properties file
 
+## Vagrant environment
+The project includes config files to run the app under Vagrant. To use them do the following:
+
+1. Build a release of the app as detailed above
+2. Install Vagrant from https://www.vagrantup.com/
+3. Create a folder to run vagrant from (eg _opwas-vagrant_)
+4. Copy the files from _src/vagrant_ into  _opwas-vagrant_
+5. Download jetty 8.1.16 .tar.gz file and place into _opwas-vagrant_ http://eclipse.org/downloads/download.php?file=/jetty/stable-8/dist/jetty-distribution-8.1.16.v20140903.tar.gz&r=1
+6. Copy the _etc_,_sql_ folders and _opwas.war_ from _release/files_ into _opwas-vagrant_
+7. Edit _opwas-vagrant_/etc/log4j-opwas.xml and change ```/play/one-page-web-app-starter/tmp/opwas.log``` to ```/vagrant/logs```
+8. From in _opwas-vagrant_ run ```vagrant up```
+9. Once the VM is running browse to http://localhost:8080/opwas/
 
 ## Development in Eclipse 
 1. Create a build using Ant as above to ensure environment is set up
--- src/vagrant/bootstrap.sh
+++ src/vagrant/bootstrap.sh
@@ -2,27 +2,44 @@
 
 apt-get update
 
+# Install mysql
+export DEBIAN_FRONTEND=noninteractive
+apt-get -q -y install mysql-server
+
+# set up database
+mysql -vv < /vagrant/sql/createdb.sql
+
 # Install Java 
 apt-get install -y openjdk-7-jdk
 
-# Install jetty
-apt-get install -y jetty8
+# make logs dir
+mkdir /vagrant/logs 
+
+# Install jetty 8 not using ubuntu package because it's JSP support is broken
+cd /tmp
+tar -xvf /vagrant/jetty-distribution-8.1.16.v20140903.tar.gz
+mv jetty-distribution-8.1.16.v20140903 /opt/jetty
 
 # configure jetty 
-echo "NO_START=0" > /etc/default/jetty8
-echo "JETTY_HOST=0.0.0.0" >> /etc/default/jetty8
-echo "JETTY_PORT=8080" >> /etc/default/jetty8
-echo "JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386/" >> /etc/default/jetty8
-echo "JAVA_OPTIONS=\"-Xmx256m -Djava.awt.headless=true -Dopwas.config.file=/opt/opwas/etc/opwas.properties\""  >> /etc/default/jetty8
+echo "NO_START=0" > /etc/default/jetty
+echo "JETTY_HOST=0.0.0.0" >> /etc/default/jetty
+echo "JETTY_PORT=8080" >> /etc/default/jetty
+echo "JETTY_HOME=/opt/jetty" >> /etc/default/jetty
+echo "JETTY_LOGS=/vagrant/logs" >> /etc/default/jetty
+echo 'JAVA_OPTIONS="-Xmx256m -Djava.awt.headless=true -Dopwas.config.file=/opt/opwas/etc/opwas.properties"' >> /etc/default/jetty
 
+# set up jetty init script
+cp /vagrant/jettyinitscript /etc/init.d/jetty
+chown root:root /etc/init.d/jetty
+chmod 744 /etc/init.d/jetty
 
-cp /vagrant/opwas.war /var/lib/jetty8/webapps/
+# set up app
+cp /vagrant/opwas.war /opt/jetty/webapps/
 mkdir /opt/opwas/
 mkdir /opt/opwas/etc
-mkdir /opt/opwas/logs
 cp /vagrant/etc/* /opt/opwas/etc/
 
-service jetty8 restart
+service jetty restart
 
 
 
